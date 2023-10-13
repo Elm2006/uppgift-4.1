1169b022b8f8896b86ba45e98ed47c090b20aac4
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-10-13 13:35:57 +0200

    Add project files.

---
+++ Uppgift 4.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgfit_4._1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for(int tal = 40; tal < 81; tal++)
+            {
+                Console.WriteLine(tal);
+            }
+            Console.ReadKey();
+        }
+    }
+}
